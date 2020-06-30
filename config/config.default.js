cf3f2d4f342604d604d5df3a03f78998cd7468bb
bianlian <bianlian@hikvision.com.cn>
2020-06-30 16:08:25 +0800

    fix: 将本地配置去除

--- config/config.default.js
+++ config/config.default.js
@@ -1,9 +1,9 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
@@ -13,35 +13,33 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {})
 
-  config.keys = appInfo.name + '_1588763657594_4897';
+  config.keys = appInfo.name + '_1588763657594_4897'
 
   config.development = {
-    watchDirs: ['lib'],
-  };
+    watchDirs: ['lib']
+  }
 
   config.static = {
-    gzip: true,
-  };
+    gzip: true
+  }
 
   config.siteFile = {
-    '/favicon.ico': fs.readFileSync(path.join(__dirname, '../app/public/favicon.ico')),
-  };
+    '/favicon.ico': fs.readFileSync(path.join(__dirname, '../app/public/favicon.ico'))
+  }
 
   config.view = {
     mapping: {
-      '.html': 'nunjucks',
-    },
-  };
+      '.html': 'nunjucks'
+    }
+  }
 
   config.security = {
     csrf: {
-      ignore: [
-        '/xapi/upload_from_xtransit',
-      ],
-    },
-  };
+      ignore: ['/xapi/upload_from_xtransit']
+    }
+  }
 
   config.multipart = {
     fileSize: '4096mb',
@@ -53,87 +51,87 @@ module.exports = appInfo => {
       '.diag',
       '.core',
       '.node',
-      '.trend',
+      '.trend'
     ],
-    mode: 'file',
-  };
+    mode: 'file'
+  }
 
   config.secure = {
-    secret: 'easy-monitor::xprofiler',
-  };
+    secret: 'easy-monitor::xprofiler'
+  }
 
-  config.httpTimeout = 15000;
+  config.httpTimeout = 15000
 
   config.profilingTime = {
     start_cpu_profiling: 5 * 60 * 1000,
     start_heap_profiling: 5 * 60 * 1000,
-    start_gc_profiling: 5 * 60 * 1000,
-  };
+    start_gc_profiling: 5 * 60 * 1000
+  }
 
-  config.profilingTimeExtra = 15 * 1000;
+  config.profilingTimeExtra = 15 * 1000
 
-  config.profilingTimeExpired = 60 * 1000;
+  config.profilingTimeExpired = 60 * 1000
 
   config.actionTime = {
     cpuprofile: {
       profilingTime: config.profilingTime.start_cpu_profiling + config.profilingTimeExtra,
-      expired: config.profilingTime.start_cpu_profiling + config.profilingTimeExpired,
+      expired: config.profilingTime.start_cpu_profiling + config.profilingTimeExpired
     },
     heapprofile: {
       profilingTime: config.profilingTime.start_heap_profiling + config.profilingTimeExtra,
-      expired: config.profilingTime.start_heap_profiling + config.profilingTimeExpired,
+      expired: config.profilingTime.start_heap_profiling + config.profilingTimeExpired
     },
     gcprofile: {
       profilingTime: config.profilingTime.start_gc_profiling + config.profilingTimeExtra,
-      expired: config.profilingTime.start_gc_profiling + config.profilingTimeExpired,
+      expired: config.profilingTime.start_gc_profiling + config.profilingTimeExpired
     },
     heapsnapshot: {
       profilingTime: config.profilingTimeExtra,
-      expired: config.profilingTimeExpired,
+      expired: config.profilingTimeExpired
     },
     diag: {
       profilingTime: config.profilingTimeExtra,
-      expired: config.profilingTimeExpired,
-    },
-  };
+      expired: config.profilingTimeExpired
+    }
+  }
 
-  config.uploadFileExpiredTime = 20 * 60 * 1000;
+  config.uploadFileExpiredTime = 20 * 60 * 1000
 
-  config.auditExpiredTime = 15 * 1000;
+  config.auditExpiredTime = 15 * 1000
 
-  config.uploadNoncePrefix = 'XTRANSIT_UPLOAD_NONCE::';
+  config.uploadNoncePrefix = 'XTRANSIT_UPLOAD_NONCE::'
 
-  const userConfig = {};
+  const userConfig = {}
   // sequelize
   userConfig.sequelize = {
     dialect: 'postgres',
-    database: 'patrolengine_patrolenginedb',
-    host: '10.19.162.225',
+    database: '',
+    host: '',
     port: 7092,
-    username: 'postgres',
-    password: 'LhXQaSzD',
-  };
+    username: '',
+    password: ''
+  }
   // mysql
   userConfig.mysql = {
     app: true,
     agent: false,
     clients: {
       xprofiler_console: {
-        host: '49.234.224.10',
+        host: '',
         port: 3306,
-        user: 'postgres',
-        password: 'Abc123+++',
-        database: 'xprofiler_console',
+        user: '',
+        password: '',
+        database: 'xprofiler_console'
       },
       xprofiler_logs: {
-        host: '49.234.224.10',
+        host: '',
         port: 3306,
-        user: 'postgres',
-        password: 'Abc123+++',
-        database: 'xprofiler_logs',
-      },
-    },
-  };
+        user: '',
+        password: '',
+        database: 'xprofiler_logs'
+      }
+    }
+  }
 
   // redis
   userConfig.redis = {
@@ -142,18 +140,18 @@ module.exports = appInfo => {
       port: 6379,
       host: '127.0.0.1',
       password: '123456',
-      db: 0,
-    },
-  };
+      db: 0
+    }
+  }
 
   // xtransit upload file
-  userConfig.xprofilerConsole = 'http://127.0.0.1:8443';
+  userConfig.xprofilerConsole = 'http://127.0.0.1:8443'
 
   // xtransit manager
-  userConfig.xtransitManager = 'http://127.0.0.1:8543';
+  userConfig.xtransitManager = 'http://127.0.0.1:8543'
 
   return {
     ...config,
-    ...userConfig,
-  };
-};
+    ...userConfig
+  }
+}
